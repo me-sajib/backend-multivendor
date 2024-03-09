f0a0f689932504a9dee57cde0522e79f0ad08667
me-sajib <me.mrsajib@gmail.com>
2024-03-10 01:34:20 +0600

    FIX auth functionality

--- schema/auth.schema.js
+++ schema/auth.schema.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 // Define a schema for the request body
 exports.registrationSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
+  username: Joi.string().min(3).max(30).required(),
+  mobile: Joi.string().min(10).max(13).required(),
   password: Joi.string().min(6).required(),
 });
--- schema/schema.sql
+++ schema/schema.sql
@@ -1,77 +1,71 @@
 CREATE TABLE `users`
 (
     `id`                        bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`                      varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `email`                     varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `email_verified_at`         timestamp NULL DEFAULT NULL,
+    `username`                  varchar(55) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `email`                     varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `mobile`                    varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `role`                      varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `is_admin`                  BOOLEAN NOT NULL DEFAULT FALSE,
     `status`                    enum('pending','verified','blocked','deleted') COLLATE
         utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
-    `otp`                       varchar(10) COLLATE utf8mb4_unicode_ci           DEFAULT NULL,
+    `address`                   varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `otp`                       varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
     `password`                  varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-    `beta_access`               tinyint                                 NOT NULL DEFAULT '0',
-    `allow_support_access`      tinyint(1) NOT NULL DEFAULT '0',
-    `is_news_letter_subscribed` tinyint(1) NOT NULL DEFAULT '1',
-    `signup_ip_address`         varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
-    `signup_source`             varchar(50) COLLATE utf8mb4_unicode_ci           DEFAULT NULL,
-    `login_ip_address`          varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
+    `token`                     varchar(255) COLLATE utf8mb4_unicode_ci          DEFAULT NULL,
     `login_at`                  datetime                                         DEFAULT NULL,
     `created_at`                timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`                timestamp NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE `users_meta`
-(
-    `id`         bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `meta_key`   varchar(255) NOT NULL,
-    `meta_value` text         NOT NULL,
-    `user_id`    bigint(20) UNSIGNED NOT NULL,
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE `websites`
+CREATE TABLE `orders`
 (
     `id`          BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `user_id`     BIGINT(20) UNSIGNED DEFAULT NULL,
-    `label`       VARCHAR(100) DEFAULT NULL,
-    `main_domain` VARCHAR(255) DEFAULT NULL,
-    `full_url`    VARCHAR(255) DEFAULT NULL,
-    `api_key`     VARCHAR(255) DEFAULT NULL,
-    `wp_version`  VARCHAR(255) DEFAULT NULL,
-    `php_version` VARCHAR(255) DEFAULT NULL,
-    `site_meta`   LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (JSON_VALID(`site_meta`)),
-    `status`      ENUM('pending','connected','disconnected','blocked','deleted') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
+    `product_ids` VARCHAR(100) DEFAULT NULL,
+    `ordered_products` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (JSON_VALID(`ordered_products`)),
+    `status`      ENUM('pending','processing','shipped','delivered','cancelled') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
+    `payment_status` ENUM('pending','paid','failed') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
+    `payment_method` ENUM('cash','bkash','nogod', 'roket', 'card') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'cash',
+    `total`       INT(11) UNSIGNED DEFAULT NULL,
+    `delivery_address` LONGTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (JSON_VALID(`delivery_address`)),
+    `delivery_date` DATE DEFAULT NULL,
     `created_at`  TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at`  TIMESTAMP NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-CREATE TABLE `tasks`
-(
-    `id`            BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `task_name`     VARCHAR(255) DEFAULT NULL,
-    `task_type`     ENUM('wordpress','shopify','blogspot') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'wordpress',
-    `user_id`       BIGINT(20) UNSIGNED DEFAULT NULL,
-    `website_id`    BIGINT(20) UNSIGNED DEFAULT NULL,
-    `status`        ENUM('pending','scheduled','running','failed','aborted','deleted','finished') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending',
-    `created_at`    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-    `scheduled_for` TIMESTAMP    DEFAULT NULL,
-    `updated_at`    TIMESTAMP    DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE `products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT,
+  `price` DECIMAL(10, 2) NOT NULL,
+  `quantity` INT NOT NULL,
+  `color`    VARCHAR(255) DEFAULT NULL,
+  `category_id` INT DEFAULT NULL,
+  `brand` VARCHAR(100),
+  `sku` VARCHAR(50),
+  `weight` DECIMAL(10, 2),
+  `images` TEXT,
+  `is_active` BOOLEAN NOT NULL DEFAULT TRUE,
+  `is_featured` BOOLEAN NOT NULL DEFAULT FALSE,
+  `rating` DECIMAL(3, 2),
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `tasks_meta`
-(
-    `id`         bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `task_id`    bigint(20) UNSIGNED NOT NULL,
-    `meta_key`   varchar(255) NOT NULL,
-    `meta_value` text         NOT NULL,
-    `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp NULL DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE `reviews` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `product_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `rating` TINYINT NOT NULL,
+  `comment` TEXT,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`product_id`) REFERENCES `products`(`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
