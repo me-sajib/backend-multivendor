9eb0e5cacecf4b1ce2585b3463c757deda30c653
me-sajib <me.mrsajib@gmail.com>
2024-03-19 03:35:16 +0600

    fix product routes

--- routes/product.route.js
+++ routes/product.route.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.post("/api/v1/product", upload.single("image"), (req, res) => {
-  const images = req.file.filename; // Binary image data from multer
+  const images = req?.file?.filename; // Binary image data from multer
 
   const {
     name,
@@ -33,6 +33,12 @@ router.post("/api/v1/product", upload.single("image"), (req, res) => {
     category,
     quantity,
   } = req.body;
+
+  // validation
+  if ((!name, !regular_price, !sale_price, !description, !quantity)) {
+    res.status(200).json({ message: "All fields are required" });
+    return;
+  }
 
   // Save image data to database
   const insertQuery = `INSERT INTO products (name, regular_price, sale_price, description, brand, category, images, quantity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
@@ -51,8 +57,7 @@ router.post("/api/v1/product", upload.single("image"), (req, res) => {
     ],
     (error, results) => {
       if (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error saving image to database" });
+        res.status(200).json({ message: "Error saving image to database" });
         return;
       }
 
@@ -72,6 +77,102 @@ router.get("/api/v1/products", (req, res) => {
     }
     if (results) {
       res.status(200).json(results);
+    }
+  });
+});
+
+router.get("/api/v1/product/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "SELECT * FROM products WHERE id = ?",
+    [id],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res
+          .status(500)
+          .json({ message: "Error retrieving data from database" });
+        return;
+      }
+      if (results) {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+router.delete("/api/v1/product/:id", (req, res) => {
+  const { id } = req.params;
+  connection.query(
+    "DELETE FROM products WHERE id = ?",
+    [id],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        res.status(500).json({
+          status: false,
+          message: "Error retrieving data from database",
+        });
+        return;
+      }
+      if (results) {
+        res
+          .status(200)
+          .json({ status: true, message: "Product deleted successfully" });
+      }
+    }
+  );
+});
+
+router.put("/api/v1/product/:id", upload.single("image"), (req, res) => {
+  const { id } = req.params;
+  const image = req?.file ? req?.file?.filename : null;
+  console.log(req.body.image);
+
+  const {
+    name,
+    regular_price,
+    sale_price,
+    description,
+    brand,
+    category,
+    quantity,
+  } = req.body;
+
+  const updateQuery = `UPDATE products SET name = ?, regular_price = ?, sale_price = ?, description = ?, brand = ?, category = ?, quantity = ? WHERE id = ?`;
+  const values = [
+    name,
+    regular_price,
+    sale_price,
+    description,
+    brand,
+    category,
+    quantity,
+    id,
+  ];
+
+  if (!name || !regular_price || !sale_price || !description || !quantity) {
+    res.status(200).json({ message: "All fields are required" });
+    return;
+  }
+
+  if (image) {
+    console.log("File deleted successfully");
+    fs.unlinkSync(`public/images/${image}`);
+    updateQuery += ", images = ?";
+    values.push(image);
+  }
+
+  connection.query(updateQuery, values, (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error retrieving data from database" });
+      return;
+    }
+    if (results) {
+      res
+        .status(200)
+        .json({ status: true, message: "Product updated successfully" });
     }
   });
 });
